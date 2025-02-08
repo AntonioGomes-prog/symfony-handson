c22ee0516c3d4659b6404ec1c74678d10c0eedfc
António Gomes <antonio.gomes@nrthree.com>
2025-02-08 16:51:53 +0000

    Author

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -41,18 +41,21 @@ class AppFixtures extends Fixture
         $microPost1->setTitle('Welcome to Portugal!');
         $microPost1->setText('Welcome to Portugal!');
         $microPost1->setCreated(new DateTime());
+        $microPost1->setAuthor($user1);
         $manager->persist($microPost1);
 
         $microPost2 = new MicroPost();
         $microPost2->setTitle('Welcome to the USA!');
         $microPost2->setText('Welcome to the USA!');
         $microPost2->setCreated(new DateTime());
+        $microPost2->setAuthor($user2);
         $manager->persist($microPost2);
 
         $microPost3 = new MicroPost();
         $microPost3->setTitle('Welcome to Germany!');
         $microPost3->setText('Welcome to Germany!');
         $microPost3->setCreated(new DateTime());
+        $microPost3->setAuthor($user1);
         $manager->persist($microPost3);
 
         $manager->flush();
